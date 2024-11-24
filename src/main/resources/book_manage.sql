2e4347b710dfa987eaf7ddf51aa53c0e12650e18
smulet <smulet@aliyun.com>
2024-11-24 20:54:28 +0800

    Demo Book Manage

---
+++ src/main/resources/book_manage.sql
@@ -0,0 +1,120 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 3307
+ Source Server Type    : MySQL
+ Source Server Version : 80012
+ Source Host           : localhost:3307
+ Source Schema         : book_manage
+
+ Target Server Type    : MySQL
+ Target Server Version : 80012
+ File Encoding         : 65001
+
+ Date: 24/11/2024 20:53:59
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'admin', '图书管理员', 'Abc123.');
+
+-- ----------------------------
+-- Table structure for book
+-- ----------------------------
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book`  (
+  `bid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`bid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of book
+-- ----------------------------
+INSERT INTO `book` VALUES (1, 'Logic', 'an intro to logic', 35.20);
+INSERT INTO `book` VALUES (2, 'The Book', 'description!', 2.99);
+INSERT INTO `book` VALUES (3, 'The Book', 'nothing', 3.99);
+
+-- ----------------------------
+-- Table structure for borrow
+-- ----------------------------
+DROP TABLE IF EXISTS `borrow`;
+CREATE TABLE `borrow`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` int(11) NULL DEFAULT NULL,
+  `bid` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique_sid_bid`(`sid` ASC, `bid` ASC) USING BTREE,
+  INDEX `f_bid`(`bid` ASC) USING BTREE,
+  CONSTRAINT `f_bid` FOREIGN KEY (`bid`) REFERENCES `book` (`bid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `f_sid` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of borrow
+-- ----------------------------
+INSERT INTO `borrow` VALUES (1, 3, 1);
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sex` enum('男','女') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `grade` int(11) NOT NULL,
+  PRIMARY KEY (`sid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, 'Ming', '男', 2019);
+INSERT INTO `student` VALUES (2, 'Ming', '男', 2019);
+INSERT INTO `student` VALUES (3, 'Ming', '男', 2019);
+INSERT INTO `student` VALUES (4, 'Ming', '男', 2019);
+INSERT INTO `student` VALUES (5, 'Luo', '男', 2019);
+INSERT INTO `student` VALUES (6, 'Hero', '女', 2020);
+INSERT INTO `student` VALUES (7, 'Hero', '男', 2021);
+INSERT INTO `student` VALUES (8, 'Nio', '女', 1024);
+INSERT INTO `student` VALUES (9, 'no', '女', 2023);
+INSERT INTO `student` VALUES (10, '罗', '女', 2024);
+
+-- ----------------------------
+-- Triggers structure for table book
+-- ----------------------------
+DROP TRIGGER IF EXISTS `del_book`;
+delimiter ;;
+CREATE TRIGGER `del_book` BEFORE DELETE ON `book` FOR EACH ROW DELETE FROM borrow WHERE bid = old.bid
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table student
+-- ----------------------------
+DROP TRIGGER IF EXISTS `del`;
+delimiter ;;
+CREATE TRIGGER `del` BEFORE DELETE ON `student` FOR EACH ROW DELETE FROM borrow WHERE sid = old.sid
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
