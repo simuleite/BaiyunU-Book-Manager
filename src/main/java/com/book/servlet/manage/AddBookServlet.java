888b3152cb07550eed368709f1d2e9caf5101488
smulet <smulet@aliyun.com>
2024-11-27 09:36:32 +0800

    Delete fail files

--- src/main/java/com/book/servlet/manage/AddBookServlet.java
+++ src/main/java/com/book/servlet/manage/AddBookServlet.java
@@ -3,7 +3,6 @@ package com.book.servlet.manage;
 import com.book.dto.ParamDTO;
 import com.book.service.BookService;
 import com.book.service.Impl.BookServiceImpl;
-import com.book.utils.RequestUtil;
 import com.book.utils.ThymeleafUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,7 +15,6 @@ import org.apache.commons.fileupload.RequestContext;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.thymeleaf.context.Context;
 
-import java.io.File;
 import java.io.IOException;
 
 @WebServlet("/add-book")
@@ -41,13 +39,7 @@ public class AddBookServlet extends HttpServlet {
         String author = req.getParameter("author");
         String desc = req.getParameter("desc");
         double price = Double.parseDouble(req.getParameter("price"));
-        if (ServletFileUpload.isMultipartContent(req)) {
-            ParamDTO dto = RequestUtil.parseParam(req);
-            FileItem image = dto.getFileMap().get("image");
-            String imagePath = this.getServletContext().getRealPath("/images");
-            String imageName = image.getName();
-            image.write(new File(imagePath + imageName));
-        }
+
         bookService.addBook(title, desc, price);
         resp.sendRedirect("books");
     }
